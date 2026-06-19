f177722d29d744318df40d71a42a1872846170fd
Vikum Samaranayake <vikum@vikum.xyz>
2026-06-20 00:40:00 +0800

    chore: add google analytics tag (#13)

--- apps/portfolio/src/pages/_app.tsx
+++ apps/portfolio/src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import '@/styles/globals.css';
 
+const GA_ID = 'G-Z4XVZ5T2GG';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
